8b9f08046fb44761b9601899a3fbec5912cb3a5f
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-12-11 16:19:59 +0100

    Add animalIsNotNullTest

--- src/test/java/test/AnimalTest.java
+++ src/test/java/test/AnimalTest.java
@@ -1,5 +1,22 @@
 package test;
 
+import main.Animal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class AnimalTest {
+    Animal animal;
+
+    @BeforeEach
+    void init() {
+        animal = new Animal("Lion");
+    }
+
+    @Test
+    void animalIsNotNullTest() {
+        assertNotNull(animal);
+    }
 
 }
