e8ca52f1a77bf50f46bd9a0515c1f6fbfd23d4d8
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-12-11 17:17:13 +0100

    Add zooSizeTest

--- src/test/java/test/ZooTest.java
+++ src/test/java/test/ZooTest.java
@@ -3,9 +3,11 @@ package test;
 import main.Zoo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ZooTest {
     Zoo zoo;
@@ -23,6 +25,22 @@ public class ZooTest {
     @Test
     void animalListIsInitiallyEmptyTest() {
         assertTrue(zoo.zooIsEmpty());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 5, 10})
+    void zooSizeTest(int arguments) {
+        int expected = arguments;
+        int actual = new Zoo(arguments).getZooSize();
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 1, 4, 6, 10})
+    void zooIsCorrectSizeTest(int arguments) {
+        int expected = arguments;
+        int actual = new Zoo(5).getZooSize();
+        assertNotEquals(expected, actual);
     }
 
 }
