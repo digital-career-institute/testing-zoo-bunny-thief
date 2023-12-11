ab0481d634a14d39df61636f32daa1c99d4612c4
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-12-11 16:57:45 +0100

    Add Zoo class

---
+++ src/main/java/main/Zoo.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.AbstractList;
+import java.util.ArrayList;
+
+public class Zoo {
+
+    private int zooSize;
+
+    public Zoo(int zooSize) {
+        this.zooSize = zooSize;
+    }
+
+}
