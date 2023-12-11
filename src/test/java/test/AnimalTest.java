8970386d9e083e0501298635ebe8a93a3946abe9
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2023-12-11 16:34:23 +0100

    Add animalNameIsValidTest

--- src/test/java/test/AnimalTest.java
+++ src/test/java/test/AnimalTest.java
@@ -4,10 +4,10 @@ import main.Animal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AnimalTest {
     Animal animal;
@@ -28,6 +28,15 @@ public class AnimalTest {
         String expected = name;
         String actual = new Animal(name).getName();
         assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"", " ", "\t", "\n", "Tiger", "lion"})
+    void animalNameIsValidTest(String argument) {
+        String expected = "Lion";
+        String actual = new Animal(argument).getName();
+        assertNotEquals(expected, actual);
     }
 
 }
